e11e5204772553a161d64de9cf4835b0250008b0
tekies0909@gmail.com <tekies0909@gmail.com>
2022-03-12 16:37:34 +0900

    cd

--- src/main/java/com/tc/springboot/repository/BoardRepository.java
+++
@@ -1,5 +0,0 @@
-package com.tc.springboot.repository;
-import com.tc.springboot.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface BoardRepository  extends JpaRepository<Board, Integer>{
-}
--- src/main/java/com/tc/springboot/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tc.springboot.repository;
-
-import com.tc.springboot.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
